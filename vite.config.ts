5cfebe53dd92cba5e816f94fcf14e8c6c9cac910
equipeway <equipeway3@gmail.com>
2025-08-30 18:24:25 -0300

    Remover SQLite e usar localStorage

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,4 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    global: 'globalThis',
-  },
-  optimizeDeps: {
-    exclude: ['better-sqlite3']
-  },
-  build: {
-    target: 'es2022'
-  },
 });
