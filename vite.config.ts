a4b2a28a086421fb509f6b6e711786b6367ab02c
equipeway <equipeway3@gmail.com>
2025-08-30 17:21:38 -0300

    Corrigir target do Vite para ES2022

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    target: 'esnext'
+    target: 'es2022'
   },
   optimizeDeps: {
     exclude: ['lucide-react'],
